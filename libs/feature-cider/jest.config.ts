e636e3839964ac62429aaa2eae67da78aba3ba2d
dosabalint <dosa.balint@gmail.com>
2022-10-23 22:01:41 +0200

    fix: dinamikus module name mapper

--- libs/feature-cider/jest.config.ts
+++ libs/feature-cider/jest.config.ts
@@ -1,3 +1,9 @@
+import {workspaceRoot} from "nx/src/utils/app-root";
+import {pathsToModuleNameMapper} from "ts-jest";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { compilerOptions } = require(workspaceRoot +'/tsconfig.base.json');
+
 /* eslint-disable */
 export default {
   displayName: 'feature-cider',
@@ -13,7 +19,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/feature-cider',
-  moduleNameMapper: {
-    '@windows-testing-error-example/feature-apple/(.*)': "<rootDir>../../libs/feature-apple/src/lib/$1"
-  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: workspaceRoot + '/' } )
 };
